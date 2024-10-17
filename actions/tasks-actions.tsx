c2e7d90ea86a0438901905f0be773d00326016c9
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-17 02:16:36 -0600

    Add task creation and deletion actions with Prisma integration

---
+++ actions/tasks-actions.tsx
@@ -0,0 +1,37 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export const createTask = async (formData: FormData) => {
+  const name = formData.get("name");
+  const description = formData.get("description");
+  const priority = formData.get("priority");
+
+  if (!name || !description || !priority) return;
+
+  await prisma.task.create({
+    data: {
+      name: name as string,
+      description: description as string,
+      priority: priority as string,
+    },
+  });
+
+  redirect("/");
+};
+
+export const deleteTask = async (formData: FormData) => {
+  const taskId = formData.get("id");
+
+  if (!taskId) return;
+
+  await prisma.task.delete({
+    where: {
+      id: parseInt(taskId as string),
+    },
+  });
+
+  revalidatePath("/");
+};
