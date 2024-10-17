0d3b4f549318cbd5612ac8adcd400b973f9821c0
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-16 23:45:51 -0600

    Add NewPage and CardWithForm components for task creation

---
+++ app/new/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CardWithForm } from "./task-form";
+
+function NewPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <CardWithForm />
+    </div>
+  );
+}
+
+export default NewPage;
